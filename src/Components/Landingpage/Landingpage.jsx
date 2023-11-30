e76b58772f7c852dce9c93199652bcc0b9c48127
kashanafzl <kashanafzal2221@gmail.com>
2023-11-30 09:42:16 +0500

    landingpage

--- src/Components/Landingpage/Landingpage.jsx
+++ src/Components/Landingpage/Landingpage.jsx
@@ -1,35 +1,80 @@
 import Carousel from 'react-bootstrap/Carousel';
-import asdImage from '../../Assets/Img/asd.png';
-import asdImage2 from '../../Assets/Img/signup.svg';
-import asdImage3 from '../../Assets/Img/background.png';
+import law1 from '../../Assets/Img/law1.png';
+import law2 from '../../Assets/Img/law2.png';
+import law3 from '../../Assets/Img/law3.png';
+import './Landingpage.css'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import legalpic from '../../Assets/Img/legalpic.png'
+import pooplepic from '../../Assets/Img/peoplepic.png'
+import sucesspic from '../../Assets/Img/sucesspic.png'
+import supportpic from '../../Assets/Img/supportpic.png'
+
 
 function IndividualIntervalsExample() {
   return (
+    <>
     <Carousel>
       <Carousel.Item interval={1000}>
-        <img src={asdImage} alt="First slide" className="d-block w-100" />
+        <img src={law1} alt="First slide"   className='law1 img-fluid' />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>We prepared to oppose for you</h3>
+          <p>Lorep ipsum dolor sit amet elit. Mauris adio mauris..</p>
+          <button>GET FREE CONSULTATION</button>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img src={asdImage2} alt="Second slide" className="d-block w-100" />
+      <Carousel.Item interval={1000}>
+        <img src={law2} alt="Second slide" className='law2  img-fluid'   />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>We prepared to oppose for you</h3>
+          <p>Lorep ipsum dolor sit amet elit. Mauris adio mauris..</p>
+          <button>GET FREE CONSULTATION</button>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img src={asdImage3} alt="Third slide" className="d-block w-100" />
+      <Carousel.Item interval={1000}>
+        <img src={law3} alt="Third slide"  className='law3  img-fluid' />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>We prepared to oppose for you</h3>
+          <p>Lorep ipsum dolor sit amet elit. Mauris adio mauris..</p>
+          <button>GET FREE CONSULTATION</button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+
+    <div className='landingpagesecsection'>
+    <Container fluid>
+      <Row>
+        <Col md={3} xs={12}>
+          <img src={legalpic} alt="" />
+          <h3>Legal</h3>
+          <p>Govt Approved</p>
+        </Col>
+        <Col md={3} xs={12}>
+        <img src={pooplepic} alt="" />
+        <h3>Attorneys</h3>
+        <p>Expert Attorneys</p>
+        </Col>
+        <Col md={3} xs={12}>
+        <img src={sucesspic} alt="" />
+        <h3>Success</h3>
+        <p>99.99% Case Won</p>
+        </Col>
+        <Col md={3} xs={12}>
+        <img src={supportpic} alt="" />
+        <h3>Support</h3>
+        <p>Quick Support</p>
+        </Col>
+      
+      </Row>
+     
+    </Container>
+    </div>
+
+      
+    </>
   );
 }
 

4a3ba68f2329513b2dbb6256d0a425c3b3878489
kashanafzl <kashanafzal2221@gmail.com>
2023-12-07 10:00:30 +0500

    navbarfooteradd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,38 +3,38 @@ import './Navbar.css'
 import logo from '../../Assets/Img/log.png'
 
 function Navbar() {
-const [toogle , setToogle] = useState(false);
+  const [toogle, setToogle] = useState(false);
 
-const showtoogle = ()=>{
+  const showtoogle = () => {
     setToogle(!toogle);
-}
+  }
   return (
     <>
-    <div className="navmain">
+      <div className="navmain">
 
-<div className="logo">
-<img src={logo} alt="" />
-</div>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
 
-<div className={toogle ? "listitems2" : "listitems"}>
-<li>Home</li>
-<li>Draft Documents</li>
-<li>Start a Busniness</li>
-<li>Media</li>
-<li>Services</li>
-<li>Blog</li>
-</div>
+        <div className={toogle ? "listitems2" : "listitems"}>
+          <li>Home</li>
+          <li>Draft Documents</li>
+          <li>Start a Busniness</li>
+          <li>Media</li>
+          <li>Services</li>
+          <li>Blog</li>
+        </div>
 
-<div className="logoutbut">
-Login
-</div>
+        <div className="logoutbut">
+          Login
+        </div>
 
-<div className="tooglebut" onClick={showtoogle}>
-    |||
-</div>
+        <div className="tooglebut" onClick={showtoogle}>
+          |||
+        </div>
 
-    </div>
-    
+      </div>
+
     </>
   )
 }
